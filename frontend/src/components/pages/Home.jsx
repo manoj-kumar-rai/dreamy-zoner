e04a6be5b6942ca64052bb81abc9196c83151b9e
manoj-kumar-rai <iammanojkumarrai@gmail.com>
2026-09-19 02:32:46 +0530

    Started

---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+<nav>
+        <div>
+          <a href="/">
+          <img
+            width="100px"
+            src="media/images/logos/DreamyZonerLogo.jpeg"
+            alt="Dreamy Zoner"
+            
+          />
+          </a>
+        </div>
+      </nav>
+  )
+}
+
+export default Home
